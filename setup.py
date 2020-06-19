5357b903248a856c15b743f71beaaeefc6b6ca75
Alisson Castro <alissoncastroskt@gmail.com>
2020-06-19 12:37:29 -0300

    ajusts url git

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A package to management migrate azure",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/alissonit/azure_migrate",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
